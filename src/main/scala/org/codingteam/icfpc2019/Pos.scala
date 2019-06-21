67e5e7e5e6ff4b788cb2122f68bbe149a0d0727c
Alexander Batischev <eual.jp@gmail.com>
2019-06-21 14:41:18 +0300

    Use BigInt in Pos

--- src/main/scala/org/codingteam/icfpc2019/Pos.scala
+++ src/main/scala/org/codingteam/icfpc2019/Pos.scala
@@ -1,4 +1,3 @@
 package org.codingteam.icfpc2019
 
-// TODO: заменить Long на какой-нибудь BigNum. Не вижу в стандартной библиотеке готовых решений, поэтому пока что Long.
-case class Pos(x: Long, y: Long)
+case class Pos(x: BigInt, y: BigInt)
